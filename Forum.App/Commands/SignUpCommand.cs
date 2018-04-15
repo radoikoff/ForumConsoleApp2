d7afe82ac4728658f0a2f0a3aa91fb4654386df0
radoikoff <radoikoff@gmail.com>
2018-04-15 23:02:01 +0300

    SignUpMenu implemented

---
+++ Forum.App/Commands/SignUpCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace Forum.App.Commands
+{
+    using Contracts;
+    using System;
+
+    public class SignUpCommand : ICommand
+    {
+        private IUserService userSevice;
+        private IMenuFactory menuFactory;
+
+        public SignUpCommand(IUserService userSevice, IMenuFactory menuFactory)
+        {
+            this.userSevice = userSevice;
+            this.menuFactory = menuFactory;
+        }
+
+        public IMenu Execute(params string[] args)
+        {
+            string username = args[0];
+            string password = args[1];
+
+            bool success = this.userSevice.TrySignUpUser(username, password);
+
+            if (success == false)
+            {
+                throw new InvalidOperationException("Invalid login!");
+            }
+
+            return this.menuFactory.CreateMenu("MainMenu");
+        }
+    }
+}
--- Forum.App/Services/UserService.cs
+++ Forum.App/Services/UserService.cs
@@ -5,6 +5,7 @@
     using Forum.DataModels;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     public class UserService : IUserService
@@ -31,12 +32,49 @@
 
         public bool TryLogInUser(string username, string password)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            User user = this.forumData.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            this.session.Reset();
+            this.session.LogIn(user);
+
+            return true;
         }
 
         public bool TrySignUpUser(string username, string password)
         {
-            throw new NotImplementedException();
+            bool validUsername = !string.IsNullOrWhiteSpace(username) && username.Length > 3;
+            bool validPassword = !string.IsNullOrWhiteSpace(password) && password.Length > 3;
+
+            if (!validUsername || !validPassword)
+            {
+                throw new ArgumentException("Username and Password must be longer than 3 symbols!");
+            }
+
+            bool userAlreadyExists = forumData.Users.Any(u => u.Username == username);
+
+            if (userAlreadyExists)
+            {
+                throw new InvalidOperationException("Username Taken!");
+            }
+
+            int userId = this.forumData.Users.LastOrDefault()?.Id + 1 ?? 1;
+            User user = new User(userId, username, password);
+
+            this.forumData.Users.Add(user);
+            this.forumData.SaveChanges();
+            this.TryLogInUser(username, password);
+
+            return true;
         }
     }
 }
